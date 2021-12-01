2400a06b6da69fa7a300b00b8e16de643b9679a0
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-12-01 17:45:03 +0100

    Generified classes

--- src/main/scala/lectures/oop/MyList.scala
+++ src/main/scala/lectures/oop/MyList.scala
@@ -1,8 +1,11 @@
 package lectures.oop
 
 import scala.language.postfixOps
+import math.Numeric.Implicits.infixNumericOps
+import math.Fractional.Implicits.infixFractionalOps
+import math.Integral.Implicits.infixIntegralOps
 
-abstract class MyList {
+abstract class MyList[+A] {
  /*
  Implement singly-linked list
  head: first element of the list
@@ -12,36 +15,37 @@ abstract class MyList {
  toString: a string representation of the list
   */
   
-  def head: Int
-  def tail: MyList
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
   def printElements: String
   // polymorphic call
   override def toString: String = "[" + printElements + "]"
 }
 
-object Empty extends MyList {
-  def head: Int = throw new NoSuchElementException
-  def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(element: Int): MyList = new Cons(element, Empty)
+  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  def head: Int = h  // returns Nothing
-  def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h  // returns Nothing
+  def tail: MyList[A] = t
   def isEmpty: Boolean = false
-  def add(element: Int): MyList = new Cons(element, this)
+  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
   def printElements: String =
-    if(t.isEmpty) "" + h
-    else h + " " + t.printElements
+    if(t.isEmpty) s"$h"
+    else s"$h ${t.printElements}"
 }
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(list.tail.head)
-  println(list.add(4).head)
-  println(list.toString)
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val listOfStrings: MyList[String] = new Cons("String", new Cons("2", new Cons("Word", Empty)))
+
+  println(listOfIntegers.toString())
+  println(listOfStrings.toString())
 }
